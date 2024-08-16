b43a930f1627e7a3accc1b8047215099a85cdbce
MelyzaRC <25936026001@ingenieria.usac.edu.gt>
2024-08-16 12:38:54 -0600

    actividad 4

---
+++ actividad4/actividad4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+while true; do
+echo "-------------------------------------"
+fecha_actual=$(date +"%d/%m/%Y")
+hora_actual=$(date +"%H:%M:%S")
+echo "Hola $fecha_actual $hora_actual"
+    sleep 1
+done
